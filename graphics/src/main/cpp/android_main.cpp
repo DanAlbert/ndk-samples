f2b32476f7cad9bfbf22980116c9a27b4f375fc3
Dan Albert <danalbert@google.com>
2024-05-16 14:11:32 -0700

    WIP: add CHECK, extract engine class

--- graphics/src/main/cpp/android_main.cpp
+++ graphics/src/main/cpp/android_main.cpp
@@ -14,59 +14,16 @@
 * limitations under the License.
 */
 
-#include <cstdlib>
-
-#include <android/looper.h>
-
-#include "game-activity/native_app_glue/android_native_app_glue.h"
-
-#define LOGI(...)
-
-static void HandleCmd(android_app *, int32_t) {
-  // Nothing for now.
-}
-
-
-static bool KeyEventFilter(const GameActivityKeyEvent *) {
-  return false;
-}
-
-static bool MotionEventFilter(const GameActivityMotionEvent *) {
-  return false;
-}
-
-static void HandleInputEvents(android_app *app) {
-  auto inputBuf = android_app_swap_input_buffers(app);
-  if (inputBuf == nullptr) {
-    return;
-  }
-
-  // For the minimum, apps need to process the exit event (for example,
-  // listening to AKEYCODE_BACK). This sample has done that in the Kotlin side
-  // and not processing other input events, we just reset the event counter
-  // inside the android_input_buffer to keep app glue code in a working state.
-  android_app_clear_motion_events(inputBuf);
-  android_app_clear_motion_events(inputBuf);
-}
-
+#include "engine.h"
+#include "logging.h"
 
 void android_main(android_app* app) {
-  app->onAppCmd = HandleCmd;
-
-  android_app_set_key_event_filter(app, KeyEventFilter);
-  android_app_set_motion_event_filter(app, MotionEventFilter);
-
-  while (!app->destroyRequested) {
-    android_poll_source* poll_source = nullptr;
-    auto result = ALooper_pollOnce(-1, nullptr, nullptr, reinterpret_cast<void**>(&poll_source));
-    if (result == ALOOPER_POLL_ERROR) {
-      std::abort();
-    }
-
-    if (poll_source != nullptr) {
-      poll_source->process(app, poll_source);
-    }
-
-    HandleInputEvents(app);
+  try {
+    ndksamples::base::InitLogging();
+    ndksamples::graphics::Engine{app}.Run();
+  } catch (const std::exception& ex) {
+    LOG(FATAL) << "Unhandled exception: " << ex.what();
+  } catch (...) {
+    LOG(FATAL) << "Unhandled exception of unknown type";
   }
 }
